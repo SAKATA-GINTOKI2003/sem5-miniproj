4a54f8a691ab7eadc65960ed01d7cee3005ab25b
SAKATA-GINTOKI2003 <147246417+SAKATA-GINTOKI2003@users.noreply.github.com>
2024-10-04 14:10:37 +0530

    fixed login issue

---
+++ build/js/login.js
@@ -0,0 +1,7 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    window.location.href='/build/home.html'
+});
+function visit() {
+window.open("signup.html", "_self");
+}
